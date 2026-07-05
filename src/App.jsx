06e4ee08b0be8ac6e22557a1f637f43a2d409c0c
Saulo Rangel <saulorangel10@hotmail.com>
2026-07-05 20:47:28 -0300

    feat: add Card, DailyBudget, and Typography components with styles; update App layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { Aside } from "./components/Aside";
 import { Container } from "./components/Container";
 import { Main } from "./components/Main";
 import { SearchInput } from "./components/Searchinput";
+import { Typography } from "./components/Typography";
+import Card from "./components/Card";
+import { DailyBudget } from "./components/DailyBudget";
 
 function App() {
   return (
@@ -10,6 +13,32 @@ function App() {
       <Aside />
       <Main>
         <SearchInput />
+        <div>
+          <Typography variant="h1">Olá, Saulo!</Typography>
+          <Typography variant="body">
+            Veja como estão suas finanças hoje.
+          </Typography>
+        </div>
+        <section>
+          <Card>
+            <Card.Header>Orçamento diário disponível:</Card.Header>
+            <Card.Body>
+              <DailyBudget value={250} />
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Header>Orçamento diário disponível:</Card.Header>
+            <Card.Body>R$ 200</Card.Body>
+          </Card>
+          <Card>
+            <Card.Header>Orçamento diário disponível:</Card.Header>
+            <Card.Body>R$ 200</Card.Body>
+          </Card>
+          <Card>
+            <Card.Header>Orçamento diário disponível:</Card.Header>
+            <Card.Body>R$ 200</Card.Body>
+          </Card>
+        </section>
       </Main>
     </Container>
   );
