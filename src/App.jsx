b9edd61d4b1f9baebc59c59b410b7c4389249708
Saulo Rangel <saulorangel10@hotmail.com>
2026-07-05 16:53:29 -0300

    feat: implement Main and SearchInput components with styles; add Icon components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import logo from './assets/logo.png'
-import { Aside } from './components/Aside';
-import { Container } from './components/Container';
+//import logo from './assets/logo.png'
+import { Aside } from "./components/Aside";
+import { Container } from "./components/Container";
+import { Main } from "./components/Main";
+import { SearchInput } from "./components/Searchinput";
 
 function App() {
   return (
     <Container>
       <Aside />
-     <h1>Poupapp</h1>
+      <Main>
+        <SearchInput />
+      </Main>
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
