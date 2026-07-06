c16cf837acde2ec6953fc02cb81a24d39ba68422
Saulo Rangel <saulorangel10@hotmail.com>
2026-07-06 07:34:14 -0300

    feat: add SavingsStatus and ProgressBar components; update App layout and styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,39 +6,45 @@ import { SearchInput } from "./components/Searchinput";
 import { Typography } from "./components/Typography";
 import Card from "./components/Card";
 import { DailyBudget } from "./components/DailyBudget";
+import { SavingsStatus } from "./components/SavingsStatus";
+import styles from "./app.module.css";
 
 function App() {
   return (
     <Container>
       <Aside />
       <Main>
-        <SearchInput />
-        <div>
-          <Typography variant="h1">Olá, Saulo!</Typography>
-          <Typography variant="body">
-            Veja como estão suas finanças hoje.
-          </Typography>
+        <div className={styles.container}>
+          <SearchInput />
+          <div>
+            <Typography variant="h1">Olá, Saulo!</Typography>
+            <Typography variant="body">
+              Veja como estão suas finanças hoje.
+            </Typography>
+          </div>
+          <section className={styles.grid}>
+            <Card>
+              <Card.Header>Orçamento diário disponível:</Card.Header>
+              <Card.Body>
+                <DailyBudget value={250} />
+              </Card.Body>
+            </Card>
+            <Card>
+              <Card.Header>Progresso da meta financeira</Card.Header>
+              <Card.Body>
+                <SavingsStatus percent={40} />
+              </Card.Body>
+            </Card>
+            <Card>
+              <Card.Header>Orçamento diário disponível:</Card.Header>
+              <Card.Body>R$ 200</Card.Body>
+            </Card>
+            <Card>
+              <Card.Header>Orçamento diário disponível:</Card.Header>
+              <Card.Body>R$ 200</Card.Body>
+            </Card>
+          </section>
         </div>
-        <section>
-          <Card>
-            <Card.Header>Orçamento diário disponível:</Card.Header>
-            <Card.Body>
-              <DailyBudget value={250} />
-            </Card.Body>
-          </Card>
-          <Card>
-            <Card.Header>Orçamento diário disponível:</Card.Header>
-            <Card.Body>R$ 200</Card.Body>
-          </Card>
-          <Card>
-            <Card.Header>Orçamento diário disponível:</Card.Header>
-            <Card.Body>R$ 200</Card.Body>
-          </Card>
-          <Card>
-            <Card.Header>Orçamento diário disponível:</Card.Header>
-            <Card.Body>R$ 200</Card.Body>
-          </Card>
-        </section>
       </Main>
     </Container>
   );
